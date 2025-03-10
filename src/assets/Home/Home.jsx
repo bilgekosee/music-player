a6b8fc72ffdd4f8487cf74a8a2a82e5fdc49d656
bilgekosee <bilge.koosee@gmail.com>
2025-03-10 23:28:34 +0300

    artist kısmı eklendi

--- src/assets/Home/Home.jsx
+++ src/assets/Home/Home.jsx
@@ -37,6 +37,50 @@ const Home = () => {
         "https://img.freepik.com/free-photo/anime-character-dj-playing-music_23-2151103481.jpg?t=st=1741565170~exp=1741568770~hmac=c4f07dac35a0dbebb84aa1712a8b3956586fc786807e2807618a7a73cb7ee79b&w=740",
     },
   ];
+  const artist = [
+    {
+      id: 1,
+      name: "bilge",
+      image:
+        "https://img.freepik.com/free-photo/anime-character-listening-music_23-2151103365.jpg?t=st=1741565965~exp=1741569565~hmac=fcdf2ca29fdebb584785a199db5b00afc962df4129383b50266b85657eae30c9&w=740",
+    },
+    {
+      id: 2,
+      name: "bilge",
+      image:
+        "https://img.freepik.com/free-photo/anime-japanese-character_23-2151478191.jpg?t=st=1741566024~exp=1741569624~hmac=1ff6b3369c3be5f729a8d4e34fd6ab5eb6abda318b1c4237909216f51d965b9a&w=740",
+    },
+    {
+      id: 3,
+      name: "bilge",
+      image:
+        "https://img.freepik.com/free-vector/hand-drawn-emo-illustration_23-2149906940.jpg?t=st=1741566054~exp=1741569654~hmac=3ff669d85d0f33f5d24dc45efdaadc05142da0b953a62742d9c86ffa883a92a2&w=740",
+    },
+    {
+      id: 4,
+      name: "bilge",
+      image:
+        "https://img.freepik.com/free-photo/anime-character-dj-playing-music_23-2151103480.jpg?t=st=1741566072~exp=1741569672~hmac=604a76f527180a7a71baa8298e249a083789bfd0880a89416ff41958c06b502d&w=740",
+    },
+    {
+      id: 5,
+      name: "bilge",
+      image:
+        "https://img.freepik.com/free-photo/anime-character-playing-guitar_23-2151103417.jpg?t=st=1741566105~exp=1741569705~hmac=70edba3b2914fe6bf8d774a9c20a1f31362b8c673bd340418612fc0843091ef4&w=740",
+    },
+    {
+      id: 6,
+      name: "bilge",
+      image:
+        "https://img.freepik.com/premium-photo/anime-boy-with-microphone-microphone-stand-front-white-background_1034478-102787.jpg?w=740",
+    },
+    {
+      id: 7,
+      name: "bilge",
+      image:
+        "https://img.freepik.com/free-photo/anime-character-dj-playing-music_23-2151103481.jpg?t=st=1741565170~exp=1741568770~hmac=c4f07dac35a0dbebb84aa1712a8b3956586fc786807e2807618a7a73cb7ee79b&w=740",
+    },
+  ];
   return (
     <div className="home-container">
       <div className="recently-play">
@@ -47,6 +91,19 @@ const Home = () => {
               <img src={item.image} alt={item.title} />
             </div>
           ))}
+        </div>
+      </div>
+      <div className="artists">
+        <div className="artist">
+          <span className="artist-span">Artist</span>
+          <div className="artists-group">
+            {artist.map((item) => (
+              <div key={item.id} className="artist-circle">
+                <img src={item.image} alt={item.title} className="artist-img" />
+                <span className="artist-name">{item.name}</span>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
